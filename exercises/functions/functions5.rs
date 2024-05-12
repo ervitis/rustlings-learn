00e5482c78a567a96e1d7d3bb5bba2afcd99fa78
ervitis <victor.martinalonso@hrbrain.co.jp>
2024-05-12 09:06:23 +0900

    exercises

--- exercises/functions/functions5.rs
+++ exercises/functions/functions5.rs
@@ -3,13 +3,16 @@
 // Execute `rustlings hint functions5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     let answer = square(3);
     println!("The square of 3 is {}", answer);
+    println!("The square of 4.5 is {}", square_2(4.5))
 }
 
 fn square(num: i32) -> i32 {
-    num * num;
+    return num * num;
+}
+
+fn square_2(num: f32) -> f32 {
+  num * num
 }
